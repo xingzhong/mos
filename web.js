4490558001edb93bade54ca56b151a6d76acf8d1
Xingzhong <xu.xeno@gmail.com>
2012-12-12 15:48:49 -0500

    db connected

--- web.js
+++ web.js
@@ -1,7 +1,26 @@
+/*
+    merchant operating system (MOS)
+    alpha v0.01
+    Apache v2 Licence 
+    Xingzhong Xu xu@yesgoody.com
+    yesgoody inc.
+    
+    roadmap
+    1. data collector supports post and read
+        POST /order 
+        GET /order
+    2. form to trigger the order
+        GET /
+        POST /
+*/
+
 var express = require('express');
 var mongoose = require('mongoose');
-var db = mongoose.createConnection('mongodb://heroku_app9889853:yesgoody2013@ds045137.mongolab.com:45137/heroku_app9889853');
+var db = mongoose.createConnection('mongodb://yesgoody_test:yesgoody2013@ds045137.mongolab.com:45137/heroku_app9889853');
 var app = express.createServer();
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/../public'));
+app.use(express.bodyParser());
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -9,22 +28,82 @@ db.once('open', function() {
 });
 
 var OrderSchema = new mongoose.Schema({
-    time: Number,
+    utc: Number,
 	type: String,
+    price : Number,
 	data: {
 		customerID : Number,
 		merchantID : Number,
-		price : Number,
-		details : 
-			[{item : String, num : Number}]
+		details : [{
+            item : String,
+            num: Number,
+        }],
 	}
 });
-
 var Order = db.model('Order', OrderSchema);
 
 app.get('/', function(request, response) {
-  response.send('main page powered by cloud9');
+    Order.find(function(err, orders){
+        if(!err){
+			response.render( 'index.jade', {title:"xingzhong", type:"GET", data:orders} );
+		}
+		else{
+			return console.log(err);
+		}
+	});
 });
+
+app.post("/", function(request, response){
+    var jsonData = JSON.parse(request.param("jsonData"));
+    var mongoData = {};
+    console.log(jsonData);
+    mongoData["utc"] = Date.now();
+    mongoData["type"] = "order";
+    mongoData["data"] = {};
+    jsonData.forEach( function (e, i, a) {
+        if (["merchantID", "customerID"].indexOf(e.name)>-1){
+            mongoData["data"][e.name] = parseInt( e.value );
+        }
+        if (["price", "tax"].indexOf(e.name)>-1){
+            mongoData[e.name] = parseFloat(e.value);
+        }
+        if (["details"].indexOf(e.name)>-1){
+            var details = []; 
+            e.value.split(",")
+                .forEach(function(detail){
+                    var d = {};
+                    d.item = detail.split(":")[0];
+                    d.num = detail.split(":")[1];
+                    details.push(d);
+                });
+            mongoData["data"]["details"] = details;
+        }
+    })
+    var order = new Order(mongoData);
+    console.log(order)
+    order.save(function (err, d) {
+        if (!err){ 
+            console.log("created!");
+            console.log(d);
+            response.send(d); 
+        }
+        else{
+            console.log("saving error");
+            console.log(err);
+        }
+	});
+});
+
+app.post('/order', function(request, response) {
+   request.on("data", function(d){
+     var order = new Order(JSON.parse(d));
+     order.save(function(err){
+         if(!err) {console.log("order created!");}
+         else {console.log("order error!");}
+     });
+   });
+});
+
 app.get('/list', function(req, res) {
 	return Order.find(function(err, orders){
 		if(!err){
